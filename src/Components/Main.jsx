befd21d46a070dbf24b9d4175457b62f4862ad8e
galicians <galicians@gmail.com>
2025-11-24 15:05:17 +0000

    background logo done

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,15 +2,16 @@ export default function Main() {
     return (
         <>
         <main className="main">
-            <h1 className="main-header">Fun facts about React</h1>
-            <ul>
-                <li className="react-fact" >Was first released in 2013</li>
-                <li className="react-fact" >Was originally created by Jordan Walke</li>
-                <li className="react-fact" >Has well over 200k stars on GitHub</li>
-                <li className="react-fact" >Is mantained by Meta</li>
-                <li className="react-fact" >Powers thousands of enterprise apps, including mobile apps</li>
+            <h1 className="facts-list-header">Fun facts about React</h1>
+            <ul className="facts-list">
+                <li className="facts-list-item" >Was first released in 2013</li>
+                <li className="facts-list-item" >Was originally created by Jordan Walke</li>
+                <li className="facts-list-item" >Has well over 200k stars on GitHub</li>
+                <li className="facts-list-item" >Is mantained by Meta</li>
+                <li className="facts-list-item" >Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
             </main>
         </>
     )
 }
+
