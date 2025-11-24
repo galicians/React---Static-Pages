99ec3dd651fa77546dd7a301b3817fdc58447e9f
galicians <galicians@gmail.com>
2025-11-24 14:11:19 +0000

    facts list items styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header.jsx';
+import Main from './Components/Main.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <Header />
-      <h1> Main component here</h1>
+      <Main />
+      {/* <h1> App jsx here</h1> */}
     </>
   )
 }
---
+++ src/Components/Main.jsx
@@ -0,0 +1,16 @@
+export default function Main() {
+    return (
+        <>
+        <main className="main">
+            <h1 className="main-header">Fun facts about React</h1>
+            <ul>
+                <li className="react-fact" >Was first released in 2013</li>
+                <li className="react-fact" >Was originally created by Jordan Walke</li>
+                <li className="react-fact" >Has well over 200k stars on GitHub</li>
+                <li className="react-fact" >Is mantained by Meta</li>
+                <li className="react-fact" >Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+            </main>
+        </>
+    )
+}
